3d398570bc8d509aa3c843fbd7f2279c895e546f
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-02 14:51:58 +0200

    feat: add single meal details page data

--- app/meals/[slug]/page.jsx
+++ app/meals/[slug]/page.jsx
@@ -1,10 +1,30 @@
 import Link from "next/link";
+import classes from './page.module.css';
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
 
 export default function MealDetailsPage({params}){
+    const meal = getMeal(params.slug);
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />')
     return (
         <>
-            <h1>Meal detail</h1>
-            <p>{params.slug}</p>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p className={classes.instructions} dangerouslySetInnerHTML={{
+                    __html: meal.instructions,
+                }}></p>
+            </main>
             <p><Link href="/">Home</Link></p>
         </>
     )
